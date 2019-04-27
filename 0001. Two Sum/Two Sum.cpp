f3a186318a484934a2d4f13c0c7312df82116d03
zizhengjiang <jiangzizheng1205@gmail.com>
2019-04-27 12:56:28 -0400

    two sum

---
+++ 0001. Two Sum/Two Sum.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &nums, int target) {
+        unordered_map<int, int> um;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (um.find(nums[i]) != um.end())
+                return {um[nums[i]], i};
+            um[target - nums[i]] = i;
+        }
+        return {-1, -1};
+    }
+}
---
+++ 0001. Two Sum/Two Sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = dict();
+        for i in range(len(nums)):
+            if nums[i] in d:
+                return [d[nums[i]], i];
+            else:
+                d[target - nums[i]] = i;
